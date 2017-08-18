79c41ad664d6534d9e7118c4c8a8822bbffab53e
ymbaek <ymbaek@se.kaist.ac.kr>
2017-08-18 23:02:46 +0900

    Initial commit

---
+++ src/com/se/HelloIntelliJ.java
@@ -0,0 +1,14 @@
+package com.se;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloIntelliJ {
+    public static void main (String []args){
+        System.out.println("Hello World");
+
+        String name = null;
+        System.out.println(StringUtils.isEmpty(name));
+    }
+
+
+}
